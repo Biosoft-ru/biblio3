24282bcc5acf5c51d6b1e09db96c9bd95b52dcf0
uuinnk <uuinnk@developmentontheedge.com>
2018-04-11 17:34:01 +0700

    add projectCategory classifications for to check access

--- src/groovy/operations/categories/Insert.groovy
+++ src/groovy/operations/categories/Insert.groovy
@@ -1,11 +1,12 @@
 package categories
 
 import com.developmentontheedge.be5.operation.GOperationSupport
+import com.developmentontheedge.be5.operation.TransactionalOperation
 
 import static ru.biosoft.biblio.Utils.BIOSTORE_PROJECTS
 
 
-class Insert extends GOperationSupport
+class Insert extends GOperationSupport implements TransactionalOperation
 {
     @Override
     Object getParameters(Map<String, Object> presetValues) throws Exception
@@ -27,16 +28,38 @@ class Insert extends GOperationSupport
     @Override
     void invoke(Object parameters) throws Exception
     {
-        if(dps.getValueAsString("parentID") == database.categories.get([name: "Root"]).getValueAsString("ID"))
+        long parentID = Long.parseLong(dps.getValueAsString("parentID"))
+        String name = dps.getValueAsString("name")
+
+        boolean isProjectCategory = parentID == (Long)database.categories.get([name: "Root"]).getValue("ID")
+
+        if(isProjectCategory)
         {
             def projects = (List<String>)session.get(BIOSTORE_PROJECTS)
-            if(!projects.contains(dps.getValueAsString("name")) && dps.getValueAsString("name") != "foo")//TODO delete foo
+            if(!projects.contains(name) && name != "foo")//TODO delete foo
             {
                 validator.setError(dps.getProperty("name"), "root category must be in " + projects.toString())
                 return
             }
         }
 
-        database.categories.add(dps)
+        def ID = database.categories.add(dps)
+
+        if(isProjectCategory)
+        {
+            database.classifications.add([
+                    recordID  : "projectCategory." + ID,
+                    categoryID: ID
+            ])
+        }
+        else
+        {
+            def id = db.insert("""INSERT INTO classifications (recordID, categoryID)
+                         SELECT CONCAT('projectCategory.', ?), c.categoryID FROM classifications c 
+                         WHERE c.recordID = CONCAT('projectCategory.', ?)""", ID, parentID)
+
+            Objects.requireNonNull(id, "projectCategory not added")
+        }
+
     }
 }
