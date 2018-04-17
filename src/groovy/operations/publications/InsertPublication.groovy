2de68360761eb69463f36cb73a9174ebc0b02f7b
uuinnk <uuinnk@developmentontheedge.com>
2018-04-17 16:10:39 +0700

    getParameters with publication2project fields

--- src/groovy/operations/publications/InsertPublication.groovy
+++ src/groovy/operations/publications/InsertPublication.groovy
@@ -1,5 +1,6 @@
 package publications
 
+import com.developmentontheedge.be5.databasemodel.RecordModel
 import com.developmentontheedge.be5.env.Inject
 import com.developmentontheedge.be5.operation.GOperationSupport
 import com.developmentontheedge.be5.operation.TransactionalOperation
@@ -12,6 +13,9 @@ import static com.developmentontheedge.be5.api.FrontendConstants.CATEGORY_ID_PAR
 class InsertPublication extends GOperationSupport implements TransactionalOperation
 {
     @Inject MedlineImport medlineImport
+
+    def projectColumns = ["status", "importance", "keyWords", "comment"]
+    RecordModel publicationRecord
 
     @Override
     Object getParameters(Map<String, Object> presetValues) throws Exception
@@ -26,16 +30,53 @@ class InsertPublication extends GOperationSupport implements TransactionalOperat
         {
             dpsHelper.addDpForColumns(dps, getInfo().getEntity(), ["PMID"], context.getOperationParams())
 
-            dps.edit("PMID") { CAN_BE_NULL = false }
+            dps.edit("PMID") {
+                CAN_BE_NULL = false
+                RELOAD_ON_CHANGE = true
+            }
+
+            if(presetValues.get("PMID") != null)
+            {
+                publicationRecord = database.publications.get([PMID: Long.parseLong((String)presetValues.get("PMID"))])
+            }
         }
         else
         {
-            dpsHelper.addDpExcludeAutoIncrement(dps, getInfo().getEntity(), context.getOperationParams())
+            dpsHelper.addDpExcludeColumns(dps, getInfo().getEntity(), ["ID","PMID"], context.getOperationParams())
         }
 
         dps.add("categoryID", "Category") {
             TAG_LIST_ATTR = helper.getTagsFromCustomSelectionView("categories", "Children Of Root", [entity: getInfo().getEntityName()])
             value = context.operationParams.get(CATEGORY_ID_PARAM)
+            RELOAD_ON_CHANGE = true
+            GROUP_ID = 2; GROUP_NAME = "Категория"
+        }
+
+        if(presetValues.get("categoryID") != null)
+        {
+            String projectID = qRec.of("""SELECT cat.name FROM categories cat
+                    INNER JOIN classifications pcls ON pcls.recordID = CONCAT('projectCategory.', ?)
+                      AND cat.ID = pcls.categoryID""", Long.parseLong((String)presetValues.get("categoryID"))).getString("name")
+
+            dpsHelper.addDpForColumns(dps, meta.getEntity("publication2project"),
+                    projectColumns, context.getOperationParams())
+
+            dps.edit("status") { CSS_CLASSES = "col-lg-6" }
+            dps.edit("importance") { CSS_CLASSES = "col-lg-6" }
+            dps.edit("keyWords") { EXTRA_ATTRS = [["inputType", "textArea"], ["rows", "1"]] as String[][] }
+
+            for (def columnName : projectColumns) {
+                dps.edit(columnName) { GROUP_ID = 1; GROUP_NAME = "Поля проекта " + projectID }
+            }
+
+            if(publicationRecord != null)
+            {
+                def publication2projectRecord = database.publication2project.get([
+                        publicationID: Long.parseLong(publicationRecord.getId()),
+                        projectID    : projectID
+                ])
+                if(publication2projectRecord != null)dpsHelper.setValues(dps, publication2projectRecord)
+            }
         }
 
         return dpsHelper.setValues(dps, presetValues)
@@ -52,24 +93,23 @@ class InsertPublication extends GOperationSupport implements TransactionalOperat
         String category = dps.getValueAsString("categoryID")
         dps.remove("categoryID")
 
-        def publicationsRecord = database.publications.get([PMID: Long.parseLong(pmid)])
-        def id
+        def publicationID
 
-        if(publicationsRecord == null)
+        if(publicationRecord == null)
         {
-            id = database.publications.add(dps)
+            publicationID = database.publications.add(dps)
 
             if(inputType == "PubMed")
             {
                 if( ( pmid != null && pmid.length() > 0 ) )
                 {
-                    medlineImport.fill("publications", Long.parseLong(id))
+                    medlineImport.fill("publications", Long.parseLong(publicationID))
                 }
             }
         }
         else
         {
-            id = Long.parseLong(publicationsRecord.getId())
+            publicationID = Long.parseLong(publicationRecord.getId())
         }
 
         List<Long> categories = new ArrayList<>()
@@ -83,11 +123,11 @@ class InsertPublication extends GOperationSupport implements TransactionalOperat
         }
 
         db.insert("""DELETE FROM classifications 
-                     WHERE recordID = CONCAT('publications.', ${id})
+                     WHERE recordID = CONCAT('publications.', ${publicationID})
                        AND categoryID IN """ + Utils.inClause(categories.size()), categories as Long[])
 
         db.insert("""INSERT INTO classifications (recordID, categoryID)
-                     SELECT CONCAT('publications.', ${id}), c.ID FROM categories c 
+                     SELECT CONCAT('publications.', ${publicationID}), c.ID FROM categories c 
                      WHERE id IN """ + Utils.inClause(categories.size()), categories as Long[])
 
         addRedirectParams(context.operationParams)
