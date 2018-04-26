0283895a7a5e8b7be645ee212ec0b46860dd4547
uuinnk <uuinnk@developmentontheedge.com>
2018-04-26 17:53:55 +0700

    move code to service, add LoadingCache for tokens

--- src/main/java/ru/biosoft/biblio/components/PubMedInfo.java
+++ src/main/java/ru/biosoft/biblio/components/PubMedInfo.java
@@ -3,44 +3,23 @@ package ru.biosoft.biblio.components;
 import com.developmentontheedge.be5.api.Component;
 import com.developmentontheedge.be5.api.Request;
 import com.developmentontheedge.be5.api.Response;
-import com.developmentontheedge.be5.api.helpers.OperationHelper;
 import com.developmentontheedge.be5.inject.Injector;
-import com.developmentontheedge.beans.DynamicPropertySet;
-import ru.biosoft.biostoreapi.DefaultConnectionProvider;
-import ru.biosoft.biostoreapi.Project;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
-
-import static java.util.stream.Collectors.toList;
-import static ru.biosoft.biblio.BiblioUtils.BIOSTORE_SERVER_NAME;
+import ru.biosoft.biblio.services.PubMedService;
 
 
 public class PubMedInfo implements Component
 {
-    private static final Logger log = Logger.getLogger(PubMedInfo.class.getName());
-
-    private OperationHelper operationHelper;
-
     @Override
     public void generate(Request req, Response res, Injector injector)
     {
-        operationHelper = injector.get(OperationHelper.class);
+        PubMedService pubMedService = injector.get(PubMedService.class);
 
-        String jwtoken = req.getNonEmpty("jwtoken");
+        String jwToken = req.getNonEmpty("jwtoken");
         String username = req.get("username");
-
-        DefaultConnectionProvider provider = new DefaultConnectionProvider(BIOSTORE_SERVER_NAME);
 
         try
         {
-            List<Project> projects = provider.getProjectListWithToken(username, jwtoken);
-
-            res.sendAsRawJson(TypedResponse.data(getData(projects)));
+            res.sendAsRawJson(TypedResponse.data(pubMedService.getData(jwToken, username)));
         }
         catch (SecurityException e)
         {
@@ -48,71 +27,28 @@ public class PubMedInfo implements Component
         }
     }
 
-    private Map<Long, PublicationProject> getData(List<Project> projects)
+    public static class TypedResponse
     {
-        List<String> projectNames = projects.stream().map(Project::getProjectName).collect(toList());
+        public String type;
+        public String message;
+        public Object data;
 
-        Map<Long, PublicationProject> publicationProjects = new HashMap<>();
-        List<DynamicPropertySet> list = operationHelper.readAsRecordsFromQuery(
-                "publications", "PubMedInfo Data", Collections.singletonMap("projects", projectNames));
-
-        for (DynamicPropertySet dps : list)
+        public static TypedResponse error(String message)
         {
-            Long pmid = dps.getValueAsLong("PMID");
-            PublicationProject publicationProject;
-
-            if(publicationProjects.containsKey(pmid))
-            {
-                publicationProject = publicationProjects.get(pmid);
-            }
-            else
-            {
-                publicationProject = new PublicationProject(dps.getValueAsLong("publicationID"), new ArrayList<>());
-                publicationProjects.put(pmid, publicationProject);
-            }
-
-            publicationProject.projects.add(new ProjectModel(
-                    dps.getValueAsLong("categoryID"),
-                    dps.getValueAsString("projectName"),
-                    dps.getValueAsString("status"),
-                    (int)dps.getValue("importance"),
-                    dps.getValueAsString("keyWords"),
-                    dps.getValueAsString("comment")
-            ));
+            return new TypedResponse("error", message, null);
         }
 
-        return publicationProjects;
-    }
-
-    public class PublicationProject
-    {
-        public final long id;
-        public final List<ProjectModel> projects;
-
-        public PublicationProject(long id, List<ProjectModel> projects)
+        public static TypedResponse data(Object data)
         {
-            this.id = id;
-            this.projects = projects;
+            return new TypedResponse("ok", null, data);
+        }
+
+        private TypedResponse(String type, String message, Object data)
+        {
+            this.type = type;
+            this.message = message;
+            this.data = data;
         }
     }
 
-    public class ProjectModel
-    {
-        public final long categoryID;
-        public final String name;
-        public final String status;
-        public final int importance;
-        public final String keyWords;
-        public final String comment;
-
-        public ProjectModel(long categoryID, String name, String status, int importance, String keyWords, String comment)
-        {
-            this.categoryID = categoryID;
-            this.name = name;
-            this.status = status;
-            this.importance = importance;
-            this.keyWords = keyWords;
-            this.comment = comment;
-        }
-    }
 }
---
+++ src/main/java/ru/biosoft/biblio/services/PubMedService.java
@@ -0,0 +1,161 @@
+package ru.biosoft.biblio.services;
+
+import com.developmentontheedge.be5.api.helpers.OperationHelper;
+import com.developmentontheedge.be5.api.services.Be5Caches;
+import com.developmentontheedge.beans.DynamicPropertySet;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import ru.biosoft.biblio.components.PubMedInfo;
+import ru.biosoft.biostoreapi.DefaultConnectionProvider;
+import ru.biosoft.biostoreapi.Project;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import static java.util.stream.Collectors.toList;
+import static ru.biosoft.biblio.BiblioUtils.BIOSTORE_SERVER_NAME;
+
+
+public class PubMedService
+{
+    private static final Logger log = Logger.getLogger(PubMedInfo.class.getName());
+
+    private static final DefaultConnectionProvider provider = new DefaultConnectionProvider(BIOSTORE_SERVER_NAME);
+    private static final LoadingCache<CacheKey, List<Project>> cache = Caffeine.newBuilder()
+            .maximumSize(1_000)
+            .expireAfterWrite(6, TimeUnit.HOURS)
+            .refreshAfterWrite(10, TimeUnit.MINUTES)
+            .recordStats()
+            .build(key -> provider.getProjectListWithToken(key.username, key.jwtoken));
+
+    private final OperationHelper operationHelper;
+
+    public PubMedService(Be5Caches be5Caches, OperationHelper operationHelper)
+    {
+        this.operationHelper = operationHelper;
+
+        be5Caches.registerCache("pubMedInfo jwtoken", cache);
+    }
+
+    public Map<Long, PublicationProject> getData(String jwtoken, String username)
+    {
+        long start = new Date().getTime();
+        List<Project> projects = cache.get(new CacheKey(username, jwtoken));
+        long time1 = new Date().getTime() - start;
+
+        long start2 = new Date().getTime();
+
+        Map<Long, PublicationProject> data = getData(projects);
+
+        log.info("PubMedInfo - " + time1 + ", " + (new Date().getTime() - start2));
+
+        return data;
+    }
+
+    private Map<Long, PublicationProject> getData(List<Project> projects)
+    {
+        List<String> projectNames = projects.stream().map(Project::getProjectName).collect(toList());
+
+        Map<Long, PublicationProject> publicationProjects = new HashMap<>();
+        List<DynamicPropertySet> list = operationHelper.readAsRecordsFromQuery(
+                "publications", "PubMedInfo Data", Collections.singletonMap("projects", projectNames));
+
+        for (DynamicPropertySet dps : list)
+        {
+            Long pmid = dps.getValueAsLong("PMID");
+            PublicationProject publicationProject;
+
+            if(publicationProjects.containsKey(pmid))
+            {
+                publicationProject = publicationProjects.get(pmid);
+            }
+            else
+            {
+                publicationProject = new PublicationProject(dps.getValueAsLong("publicationID"), new ArrayList<>());
+                publicationProjects.put(pmid, publicationProject);
+            }
+
+            publicationProject.projects.add(new ProjectModel(
+                    dps.getValueAsLong("categoryID"),
+                    dps.getValueAsString("projectName"),
+                    dps.getValueAsString("status"),
+                    (int)dps.getValue("importance"),
+                    dps.getValueAsString("keyWords"),
+                    dps.getValueAsString("comment")
+            ));
+        }
+
+        return publicationProjects;
+    }
+
+    public class PublicationProject
+    {
+        public final long id;
+        public final List<ProjectModel> projects;
+
+        public PublicationProject(long id, List<ProjectModel> projects)
+        {
+            this.id = id;
+            this.projects = projects;
+        }
+    }
+
+    public class ProjectModel
+    {
+        public final long categoryID;
+        public final String name;
+        public final String status;
+        public final int importance;
+        public final String keyWords;
+        public final String comment;
+
+        public ProjectModel(long categoryID, String name, String status, int importance, String keyWords, String comment)
+        {
+            this.categoryID = categoryID;
+            this.name = name;
+            this.status = status;
+            this.importance = importance;
+            this.keyWords = keyWords;
+            this.comment = comment;
+        }
+    }
+
+    class CacheKey
+    {
+        public String username;
+        public String jwtoken;
+
+        public CacheKey(String username, String jwtoken)
+        {
+            this.username = username;
+            this.jwtoken = jwtoken;
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            CacheKey cacheKey = (CacheKey) o;
+
+            if (username != null ? !username.equals(cacheKey.username) : cacheKey.username != null) return false;
+            return jwtoken != null ? jwtoken.equals(cacheKey.jwtoken) : cacheKey.jwtoken == null;
+        }
+
+        @Override
+        public int hashCode()
+        {
+            int result = username != null ? username.hashCode() : 0;
+            result = 31 * result + (jwtoken != null ? jwtoken.hashCode() : 0);
+            return result;
+        }
+    }
+
+}
