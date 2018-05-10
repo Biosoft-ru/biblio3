d551efbec8a6a9fc3e109e0aa0cc7cbe2477efbb
uuinnk <uuinnk@developmentontheedge.com>
2018-05-10 13:29:05 +0700

    prevent create duplicate

--- src/groovy/operations/citations/Insert.groovy
+++ src/groovy/operations/citations/Insert.groovy
@@ -1,5 +1,6 @@
 package citations
 
+import com.developmentontheedge.be5.databasemodel.RecordModel
 import com.developmentontheedge.be5.model.Base64File
 import com.developmentontheedge.be5.operation.TransactionalOperation
 import com.developmentontheedge.be5.operation.support.GOperationSupport
@@ -29,35 +30,48 @@ class Insert extends GOperationSupport implements TransactionalOperation
     {
         def file = (Base64File) dps.$file
 
-        CSL citeproc = new CSL(new DummyProvider(), new String(file.data))
-        citeproc.setOutputFormat("html")
+        def name = file.name.replace('.csl', '')
 
-        citeproc.registerCitationItems("ID-1", "ID-2", "ID-3", "ID-4", "ID-5")
+        RecordModel<Long> rec = database.citations.getBy([name: name])
 
-        def line1 = citeproc.makeCitation("ID-1").get(0).getText()
-        def line2 = citeproc.makeCitation("ID-1", "ID-2").get(0).getText()
-        def line3 = citeproc.makeCitation("ID-1", "ID-2", "ID-5").get(0).getText()
+        Long id
+        if(rec == null)
+        {
+            CSL citeproc = new CSL(new DummyProvider(), new String(file.data))
+            citeproc.setOutputFormat("html")
 
-        Bibliography bibl = citeproc.makeBibliography()
+            citeproc.registerCitationItems("ID-1", "ID-2", "ID-3", "ID-4", "ID-5")
 
-        def id = database.citations.add([
-                name: file.name.replace('.csl', ''),
-                title: dps.$title,
-                inline: "${line1}<br/>${line2}<br/>${line3}".toString(),
-                bibliography: (bibl.getEntries()[0] + bibl.getEntries()[1])
-        ])
+            def line1 = citeproc.makeCitation("ID-1").get(0).getText()
+            def line2 = citeproc.makeCitation("ID-1", "ID-2").get(0).getText()
+            def line3 = citeproc.makeCitation("ID-1", "ID-2", "ID-5").get(0).getText()
+
+            Bibliography bibl = citeproc.makeBibliography()
+
+            id = database.citations.add([
+                    name: name,
+                    title: dps.$title,
+                    inline: "${line1}<br/>${line2}<br/>${line3}".toString(),
+                    bibliography: (bibl.getEntries()[0] + bibl.getEntries()[1])
+            ])
+
+            database.attachments.add([
+                    ownerID : "citations." + id,
+                    name    : file.name,
+                    data    : file.data,
+                    mimeType: file.mimeTypes,
+                    type    : "other"
+            ])
+        }
+        else
+        {
+            id = rec.getPrimaryKey()
+        }
 
         database.citation2user.add([
                 citationID: id,
                 user_name: userInfo.getUserName()
         ])
 
-        database.attachments.add([
-                ownerID : "citations." + id,
-                name    : file.name,
-                data    : file.data,
-                mimeType: file.mimeTypes,
-                type    : "other"
-        ])
     }
 }
