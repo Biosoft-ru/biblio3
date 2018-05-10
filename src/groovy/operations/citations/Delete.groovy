fcbbf7d2d9ad3ee45a67c9b5c0d8289d6e68b2cc
uuinnk <uuinnk@developmentontheedge.com>
2018-05-10 14:19:58 +0700

    add Delete

---
+++ src/groovy/operations/citations/Delete.groovy
@@ -0,0 +1,26 @@
+package citations
+
+import com.developmentontheedge.be5.operation.TransactionalOperation
+import com.developmentontheedge.be5.operation.support.GOperationSupport
+
+
+class Delete extends GOperationSupport implements TransactionalOperation
+{
+    @Override
+    Object getParameters(Map<String, Object> presetValues) throws Exception
+    {
+        return null
+    }
+
+    @Override
+    void invoke(Object parameters) throws Exception
+    {
+        for(def id : context.records)
+        {
+            database.citation2user.removeBy([
+                    citationID: id,
+                    user_name: userInfo.getUserName()
+            ])
+        }
+    }
+}
