018ab7a3571ef359376718f290c02ab61d233723
uuinnk <uuinnk@developmentontheedge.com>
2018-05-14 20:01:20 +0700

    link in modal - in process

--- src/groovy/operations/publications/Bibliography.groovy
+++ src/groovy/operations/publications/Bibliography.groovy
@@ -1,10 +1,17 @@
 package publications
 
+import com.developmentontheedge.be5.model.StaticPagePresentation
+import com.developmentontheedge.be5.model.jsonapi.JsonApiModel
+import com.developmentontheedge.be5.model.jsonapi.ResourceData
+import com.developmentontheedge.be5.operation.OperationResult
 import com.developmentontheedge.be5.operation.support.GOperationSupport
+import com.developmentontheedge.be5.util.HashUrlUtils
 
 import java.util.stream.Collectors
 
 import static com.developmentontheedge.be5.api.FrontendActions.*
+import static com.developmentontheedge.be5.api.FrontendConstants.STATIC_ACTION
+import static com.developmentontheedge.be5.api.RestApiConstants.SELF_LINK
 
 
 class Bibliography extends GOperationSupport
@@ -34,6 +41,16 @@ class Bibliography extends GOperationSupport
                 "type=${dps.getValue("type")}&publicationIDs=${ids}&citationFileID=${attID}"
 
 
+        //setResult(OperationResult.redirect())
+
+        //redirectThisOperation()
+        //HashUrlUtils.getUrl(this).toString()
+
+        setResult(OperationResult.finished(null, [updateDocument(
+                JsonApiModel.data(new ResourceData(STATIC_ACTION,
+                        new StaticPagePresentation("title", "test"),
+                        Collections.emptyMap()), "")
+        )]))
 
         //setResult(OperationResult.finished("Download Will Start Shortly", [redirect(url), goBack()]))
 
