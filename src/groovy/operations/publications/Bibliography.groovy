9b342fb9a7c45462d21189abd6325e80139e292e
uuinnk <uuinnk@developmentontheedge.com>
2018-05-15 12:07:40 +0700

    open StaticPage with links

--- src/groovy/operations/publications/Bibliography.groovy
+++ src/groovy/operations/publications/Bibliography.groovy
@@ -1,21 +1,19 @@
 package publications
 
-import com.developmentontheedge.be5.model.StaticPagePresentation
-import com.developmentontheedge.be5.model.jsonapi.JsonApiModel
-import com.developmentontheedge.be5.model.jsonapi.ResourceData
+import com.developmentontheedge.be5.api.services.DocumentGenerator
 import com.developmentontheedge.be5.operation.OperationResult
 import com.developmentontheedge.be5.operation.support.GOperationSupport
-import com.developmentontheedge.be5.util.HashUrlUtils
+import com.google.inject.Inject
 
 import java.util.stream.Collectors
 
 import static com.developmentontheedge.be5.api.FrontendActions.*
-import static com.developmentontheedge.be5.api.FrontendConstants.STATIC_ACTION
-import static com.developmentontheedge.be5.api.RestApiConstants.SELF_LINK
 
 
 class Bibliography extends GOperationSupport
 {
+    @Inject DocumentGenerator documentGenerator
+
     @Override
     Object getParameters(Map<String, Object> presetValues) throws Exception
     {
@@ -40,21 +38,12 @@ class Bibliography extends GOperationSupport
         String url = request.getBaseUrl() + "/api/bibliography?" +
                 "type=${dps.getValue("type")}&publicationIDs=${ids}&citationFileID=${attID}"
 
+        def content = meta.getStaticPageContent(userInfo.getLanguage(), "bibliography.be")
+                .replace("OPEN_URL", url + "&_download_=no")
+                .replace("DOWNLOAD_URL", url + "&_download_=yes")
 
-        //setResult(OperationResult.redirect())
-
-        //redirectThisOperation()
-        //HashUrlUtils.getUrl(this).toString()
-
-        setResult(OperationResult.finished(null, [updateDocument(
-                JsonApiModel.data(new ResourceData(STATIC_ACTION,
-                        new StaticPagePresentation("title", "test"),
-                        Collections.emptyMap()), "")
-        )]))
-
-        //setResult(OperationResult.finished("Download Will Start Shortly", [redirect(url), goBack()]))
-
-        //window.open blocked by browser usually
-        //setResult(OperationResult.finished(null, [goBack(), openNewWindow(url)]))
+        setResult(OperationResult.finished(null, updateDocument(
+                documentGenerator.getStaticPage("Bibliography", content, null)
+        )))
     }
 }
