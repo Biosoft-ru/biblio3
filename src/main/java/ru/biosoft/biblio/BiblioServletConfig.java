55e4d9d946c078de682794b89ca66599395a9d64
uuinnk <uuinnk@developmentontheedge.com>
2018-05-21 14:43:21 +0700

    add support class for guice Stage

--- src/main/java/ru/biosoft/biblio/BiblioServletConfig.java
+++ src/main/java/ru/biosoft/biblio/BiblioServletConfig.java
@@ -2,19 +2,19 @@ package ru.biosoft.biblio;
 
 import com.developmentontheedge.be5.ServerModule;
 import com.developmentontheedge.be5.modules.core.CoreModule;
+import com.developmentontheedge.be5.servlet.Be5GuiceServletContextListener;
 import com.developmentontheedge.be5.servlet.TemplateModule;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.servlet.GuiceServletContextListener;
 
 
-public class BiblioServletConfig extends GuiceServletContextListener
+public class BiblioServletConfig extends Be5GuiceServletContextListener
 {
     @Override
     protected Injector getInjector()
     {
-        return Guice.createInjector(
+        return Guice.createInjector(getStage(),
                 new ServerModule(),
                 new CoreModule(),
                 new TemplateModule(),
