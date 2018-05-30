0908e391bd591f65079f262c1774b48df7432734
uuinnk <uuinnk@developmentontheedge.com>
2018-05-30 16:09:05 +0700

    fix tests

--- src/test/groovy/operations/publications/InsertPublicationTest.groovy
+++ src/test/groovy/operations/publications/InsertPublicationTest.groovy
@@ -24,7 +24,7 @@ class InsertPublicationTest extends Biblio3Test
 
         Object first = generateOperation("publications", "Compact view", "Insert", "").getFirst()
 
-        assertEquals("{'values':{'inputType':'PubMed','PMID':'','categoryID':''},'meta':{'/inputType':{'displayName':'Ввод','reloadOnChange':true,'cssClasses':'col-lg-4','tagList':[['PubMed','PubMed'],['manually','Вручную']]},'/PMID':{'displayName':'PMID','type':'Long','reloadOnChange':true,'validationRules':[{'attr':{'max':'9223372036854775807','min':'0'},'type':'range'},{'attr':'1','type':'step'}]},'/categoryID':{'displayName':'Category','type':'Long','reloadOnChange':true,'groupId':'2','groupName':'Категория','tagList':[]}},'order':['/inputType','/PMID','/categoryID']}",
+        assertEquals("{'values':{'inputType':'PubMed','PMID':'','categoryID':''},'meta':{'/inputType':{'displayName':'Input type','reloadOnChange':true,'cssClasses':'col-lg-4','tagList':[['PubMed','PubMed'],['manually','Вручную']]},'/PMID':{'displayName':'PMID','type':'Long','reloadOnChange':true,'validationRules':[{'attr':{'max':'9223372036854775807','min':'0'},'type':'range'},{'attr':'1','type':'step'}]},'/categoryID':{'displayName':'Category','type':'Long','reloadOnChange':true,'groupId':'2','groupName':'Category','tagList':[]}},'order':['/inputType','/PMID','/categoryID']}",
                 oneQuotes(JsonFactory.bean(first)))
     }
 
