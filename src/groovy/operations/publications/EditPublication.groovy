09b621fe93f14ba431f2b811273890ca97f6d232
Innokentiy <uu.innk@gmail.com>
2018-11-17 22:02:44 +0700

    make @TypeChecked

--- src/groovy/operations/publications/EditPublication.groovy
+++ src/groovy/operations/publications/EditPublication.groovy
@@ -5,8 +5,9 @@ import com.developmentontheedge.be5.databasemodel.RecordModel
 import com.developmentontheedge.be5.base.model.GDynamicPropertySetSupport
 import com.developmentontheedge.be5.operation.model.OperationResult
 import com.developmentontheedge.be5.operation.model.TransactionalOperation
+import groovy.transform.TypeChecked
 
-
+@TypeChecked
 class EditPublication extends InsertPublication implements TransactionalOperation
 {
     RecordModel publication2projectRecord
@@ -28,9 +29,9 @@ class EditPublication extends InsertPublication implements TransactionalOperatio
             return null
         }
 
-        publicationRec = database.publications.get(context.record)
+        publicationRec = database["publications"].get(context.record)
 
-        publication2projectRecord = database.publication2project.getBy([
+        publication2projectRecord = database["publication2project"].getBy([
                 publicationID: publicationRec.getPrimaryKey(),
                 projectID    : projectID
         ])
@@ -67,10 +68,10 @@ class EditPublication extends InsertPublication implements TransactionalOperatio
         def projectInfo = extractProjectInfo(params)
         projectInfo.add("publicationID"){TYPE = Long; value = context.records[0]}
 
-        database.publications.set(context.record, params)
+        database["publications"].set(context.record, params)
 
         if(publication2projectRecord == null){
-            database.publication2project.add(projectInfo)
+            database["publication2project"].add(projectInfo)
         }else{
             publication2projectRecord.update(projectInfo.asMap())
         }
