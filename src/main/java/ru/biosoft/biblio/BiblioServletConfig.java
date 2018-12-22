dc017a92a6b79fa69fd3cde57b86bfe50ce7982c
Innokentiy <uu.innk@gmail.com>
2018-12-23 02:29:32 +0700

    add Bootstrap stub, rename Be5GuiceServletContextListener -> Be5ServletListener

--- src/main/java/ru/biosoft/biblio/BiblioServletConfig.java
+++ src/main/java/ru/biosoft/biblio/BiblioServletConfig.java
@@ -2,14 +2,14 @@ package ru.biosoft.biblio;
 
 import com.developmentontheedge.be5.modules.core.CoreModule;
 import com.developmentontheedge.be5.server.ScopedServerModule;
-import com.developmentontheedge.be5.server.servlet.Be5GuiceServletContextListener;
+import com.developmentontheedge.be5.server.servlet.Be5ServletListener;
 import com.developmentontheedge.be5.server.servlet.TemplateModule;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 
-public class BiblioServletConfig extends Be5GuiceServletContextListener
+public class BiblioServletConfig extends Be5ServletListener
 {
     @Override
     protected Injector getInjector()
